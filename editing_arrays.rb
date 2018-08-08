a510c3094260aa9da4806006bd8454c44f0dc1af
graciemzhang <gracie.zhang@lps-students.org>
2018-08-08 18:14:58 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -13,8 +13,8 @@ def change_rainbow_colors
 ]
 
   rainbow_colors[0] = "red"
-  rainbow_colors[1] = "light red"
-  rainbow_colors[2] = "light yellow"
+  rainbow_colors[1] = "light_red"
+  rainbow_colors[2] = "light_yellow"
 end
 
 def add_colors
